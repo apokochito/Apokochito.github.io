ce8f47cd26b5643e6fbca49e4457ef37b6abfb34
Apokochito <dpinto1@ucol.mx>
2020-09-18 12:18:50 -0500

    Update CV

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import "./../assets/css/Intro.css";
 class Intro extends Component {
   btnClick() {
     window.open(
-      "https://www.dropbox.com/s/6f7r27cg6012tj5/Diana%20Pinto%20CV.docx?dl=0"
+      "https://www.dropbox.com/s/72i9oiwmi7xw4bi/Diana%20Pinto%20CV.docx?dl=0"
     );
   }
   render() {
